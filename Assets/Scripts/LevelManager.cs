eaf5919c106b310be6b4eb76dba6da9833cfb3bd
mccaua79 <drewmccauley90@gmail.com>
2020-03-08 22:24:45 -0500

    Adding level manager which will house the brick layout designs, and what levels to build onto the scene

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager _instance = null;
+
+    public LevelManager()
+    {
+
+    }
+
+    public static LevelManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new LevelManager();
+            }
+
+            return _instance;
+        }
+    }
+}
